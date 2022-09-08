f4a7c99476d46887087d7904cf0825b6d19c71ce
Murat Aydin <muratabinpro@gmail.com>
2022-09-08 16:48:14 +0300

    Create Technology validator

---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/Technologies/Commands/CreateTechnology/CreateTechnologyCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Technologies.Commands.CreateTechnology;
+
+public class CreateTechnologyCommandValidator : AbstractValidator<CreateTechnologyCommand>
+{
+    public CreateTechnologyCommandValidator()
+    {
+        RuleFor(c => c.ProgrammingLanguageId).NotEmpty();
+        RuleFor(c => c.Name).NotEmpty();
+    }
+}
