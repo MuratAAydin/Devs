0da31c10a8488dba29124cb0b9070a6968e6e4c1
Murat Aydin <muratabinpro@gmail.com>
2022-09-09 21:54:10 +0300

    GitHub connection repositories

---
+++ src/demoProjects/Kodlama.io.Devs/Application/Services/Repositories/IGitHubConnectionRepository.cs
@@ -0,0 +1,8 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface IGitHubConnectionRepository : IAsyncRepository<GitHubConnection>, IRepository<GitHubConnection>
+{
+}
