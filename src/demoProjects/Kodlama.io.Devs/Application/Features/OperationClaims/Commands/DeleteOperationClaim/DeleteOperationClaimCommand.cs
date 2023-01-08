f268723336b0fc4ef606e0fccc50f15055d67e69
Murat Aydin <muratabinpro@gmail.com>
2023-01-09 00:47:34 +0300

    Delete operation claim CQRS

---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/OperationClaims/Commands/DeleteOperationClaim/DeleteOperationClaimCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.OperationClaims.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Security.Entities;
+using MediatR;
+
+namespace Application.Features.OperationClaims.Commands.DeleteOperationClaim;
+
+public class DeleteOperationClaimCommand : IRequest<DeletedOperationClaimDto>
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+    public class
+        DeleteOperationClaimCommandHandler : IRequestHandler<DeleteOperationClaimCommand, DeletedOperationClaimDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IOperationClaimRepository _operationClaimRepository;
+
+        public DeleteOperationClaimCommandHandler(IMapper mapper,
+            IOperationClaimRepository operationClaimRepository)
+        {
+            _mapper = mapper;
+            _operationClaimRepository = operationClaimRepository;
+        }
+
+        public async Task<DeletedOperationClaimDto> Handle(DeleteOperationClaimCommand request,
+            CancellationToken cancellationToken)
+        {
+            OperationClaim mappedOperationClaim = _mapper.Map<OperationClaim>(request);
+            OperationClaim createdOperationClaim = await _operationClaimRepository.AddAsync(mappedOperationClaim);
+            DeletedOperationClaimDto createdOperationClaimDto =
+                _mapper.Map<DeletedOperationClaimDto>(createdOperationClaim);
+
+            return createdOperationClaimDto;
+        }
+    }
+}
---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/OperationClaims/Commands/DeleteOperationClaim/DeleteOperationClaimCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Features.OperationClaims.Commands.DeleteOperationClaim;
+
+public class DeleteOperationClaimCommandValidator : AbstractValidator<DeleteOperationClaimCommand>
+{
+    public DeleteOperationClaimCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty();
+    }
+}
